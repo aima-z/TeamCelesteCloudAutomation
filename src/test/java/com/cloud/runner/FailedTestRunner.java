e3c2b0ec02431c009cfa4e25e849860d26ef1de3
collins <collinskipkoech95@gmail.com>
2023-02-28 19:40:22 +0300

    first commit

---
+++ src/test/java/com/cloud/runner/FailedTestRunner.java
@@ -0,0 +1,13 @@
+package com.cloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/cloud/step_definitions"
+)
+public class FailedTestRunner {
+}
