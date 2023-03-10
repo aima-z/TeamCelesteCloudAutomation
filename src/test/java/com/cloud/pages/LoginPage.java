d68e9814183ff59bb88d00f55541238450bae9b5
lbfarhat <114306596+lbfarhat@users.noreply.github.com>
2023-03-09 23:51:54 -0600

    Update LoginPage.java

--- src/test/java/com/cloud/pages/LoginPage.java
+++ src/test/java/com/cloud/pages/LoginPage.java
@@ -7,26 +7,21 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import static com.cloud.utilities.Driver.getDriver;
-
 public class LoginPage {
-
+    
     public LoginPage(){
-
+        
         PageFactory.initElements(Driver.getDriver(), this);
     }
-
+    
     @FindBy(id="user")
     public WebElement userName;
-
-
+    
     @FindBy(id="password")
     public WebElement password;
 
     @FindBy(id = "submit-form")
     public WebElement submit;
-
-
-
 
     public void login(String userNameStr, String passwordStr) {
         userName.sendKeys(userNameStr);
