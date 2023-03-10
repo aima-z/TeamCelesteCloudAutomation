2c3dbf24b444a1f0dc464d6d608f3833da31df03
Kseniame <mksenia28@gmail.com>
2023-03-09 23:24:00 -0600

    add commit

--- src/test/java/com/cloud/runner/CukesRunner.java
+++ src/test/java/com/cloud/runner/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cloud/step_definitions",
         dryRun = true,
-        tags = "",
+        tags = " ",
         publish = false //generating a report with public link
 )
 public class CukesRunner {}
