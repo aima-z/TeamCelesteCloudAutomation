a5a0a7f02af720ab9aa405bf3195831d39ecbb9c
IsraaAmeen <israa.ameen@yahoo.com>
2023-03-10 06:09:45 -0600

    Israa US complete

---
+++ src/test/java/com/cloud/pages/PhotosPage.java
@@ -0,0 +1,22 @@
+package com.cloud.pages;
+
+import com.cloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PhotosPage {
+
+   public PhotosPage() {
+       PageFactory.initElements(Driver.getDriver(), this);
+   }
+
+       @FindBy(xpath = "//ul[@class='app-navigation__list']")
+
+       public WebElement ListOfSubModules;
+   }
+
+
+
+
+
