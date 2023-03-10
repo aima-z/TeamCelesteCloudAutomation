6e1f9e72b471511d231374a4cff940b01ec4c02d
lbfarhat <114306596+lbfarhat@users.noreply.github.com>
2023-03-09 23:50:25 -0600

    Update CukesRunner.java

--- src/test/java/com/cloud/runner/CukesRunner.java
+++ src/test/java/com/cloud/runner/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cloud/step_definitions",
-        dryRun = false,
-        tags = "@WIP",
+        dryRun = true,
+        tags = "@Login",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
